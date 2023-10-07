98f6c50031aac0a27f67556e408dea221b3cadf4
Ec3o <144577078+Ec3o@users.noreply.github.com>
2023-10-07 19:02:58 +0800

    Update error.go

--- TODOlist/error.go
+++ TODOlist/error.go
@@ -1,18 +1,46 @@
 package main
 
-// TODOError 此文件用于预定义错误类型
+//此文件用于预定义错误类型
+
+// TODOError TODO错误类型
 type TODOError struct {
 	Message string `json:"error"`
 }
 
+// USERError 用户错误类型
+type USERError struct {
+	Message string `json:"error"`
+}
+
+// Successes 成功操作类型
+type Successes struct {
+	Message string `json:"status"`
+}
+
+// TODOError
 var (
 	ErrInvalidTODOFormat = TODOError{"抱歉，您提供的TODO数据格式不正确"}
-	ErrInvalidDeadline   = TODOError{"无效的截止时间，格式应为 '2006-01-02' 并且不能是过去的时间"}
+	ErrInvalidDeadline   = TODOError{"无效的截止时间"}
 	ErrReadTODOData      = TODOError{"无法读取TODO数据"}
 	ErrSaveTODOData      = TODOError{"无法保存TODO数据"}
 	ErrTODOIndexNotExist = TODOError{"抱歉，您访问的ToDo目前不存在，请先创建"}
 	ErrTODONotFound      = TODOError{"抱歉，您要删除的ToDo目前不存在，请先创建"}
-	ErrNoTodayTODO       = TODOError{"今天没有截止的TODO"}
-	ErrNoWeekTODO        = TODOError{"本周没有截止的TODO"}
-	ErrNoUnfinishedTODO  = TODOError{"没有未完成的TODO"}
+)
+
+// USERError
+var (
+	ErrInvalidUSERFormat = USERError{"抱歉，您提供的用户数据格式不正确"}
+	ErrInvalidPassword   = USERError{"密码不能为空或密码长度过短"}
+	ErrReadUserData      = USERError{"无法读取用户数据"}
+	ErrSaveUserData      = USERError{"无法保存保存数据"}
+	ErrUserlogin         = USERError{"用户未注册或密码错误"}
+	ErrUser              = USERError{"用户未登录或无效用户"}
+	ErrRegister          = USERError{"用户名已经被注册"}
+)
+
+// Successes
+var (
+	TodoSubmitSuccess   = Successes{"数据提交成功"}
+	UserRegisterSuccess = Successes{"用户注册成功"}
+	TodoDeleteSuccess   = Successes{"数据删除成功"}
 )
